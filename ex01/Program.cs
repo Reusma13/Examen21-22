552defae1e34c9cb2d144c936d5d9f246e012cd4
Reusma13 <a2308764@institutmontilivi.cat>
2024-02-06 10:35:33 +0100

    Començament del primer exercici

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array1 = { 5, 31, 6, 13, 4, 7, 2, 45, 22, 1 };
+            Console.WriteLine("Cuantos nElem quieres: ");
+            int nElem = Convert.ToInt32(Console.ReadLine());
+            int[] array2 = CreateArray2(array1.Length, nElem);
+
+            Console.WriteLine("Array1: " + string.Join(", ", array1));
+            Console.WriteLine("Array2: " + string.Join(", ", array2));
+        }
+
+        static int[] CreateArray2(int mida, int nElem)
+        {
+            Random rand = new Random();
+            int[] array2 = new int[nElem];
+
+            for (int i = 0; i < nElem; i++)
+            {
+                array2[i] = rand.Next(mida);
+            }
+
+            Array.Sort(array2);
+
+            return array2;
         }
     }
 }
